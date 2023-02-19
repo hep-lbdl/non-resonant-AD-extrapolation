b64a05fcf6fc0c3fa3060c052a3ae2148e774aa1
Kehang Bai <kehangbai@gmail.com>
2023-02-19 14:05:09 -0800

    Update to pytorch.

---
+++ SimpleMAF.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve
+
+from tqdm import tqdm
+
+from nflows.flows.base import Flow
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.base import CompositeTransform
+from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
+from nflows.transforms.permutations import ReversePermutation
+
+
+"""
+A simple implementation of a Masked Autoregressive Flow (MAF).
+
+Author: Kehang Bai
+Date: Feb 19, 2023
+"""
+
+class SimpleMAF:
+    def __init__(self,
+                 num_features,
+                 num_cond_features=None,
+                 num_hidden_features=4,
+                 num_layers=5,
+                 act='relu',
+                 device='cpu'):
+        
+        activations = {'relu': F.relu, 'sigmoid': F.sigmoid, 'tanh': F.tanh}
+        activation = activations[act]
+        
+        self.nfeat = num_features
+        self.ncond = num_cond_features
+        base_dist = StandardNormal(shape=[num_features])
+
+        transforms = []
+        for _ in range(num_layers):
+            transforms.append(ReversePermutation(features=num_features))
+            transforms.append(MaskedAffineAutoregressiveTransform(features=num_features, 
+                                                                  hidden_features=num_hidden_features, 
+                                                                  context_features=num_cond_features, 
+                                                                  activation = activation))
+        transform = CompositeTransform(transforms)
+        self.flow = Flow(transform, base_dist)
+        self.optimizer = optim.Adam(self.flow.parameters())
+        self.device = device
+
+    def np_to_torch(self, array):
+    
+        return torch.tensor(array.astype(np.float32)).to(self.device)
+    
+    def process_data(self, data, batch_size, cond=None):
+        
+        if self.nfeat != data.ndim:
+            raise RuntimeError("input data dimention doesn't match with number of features!")
+        if self.nfeat == 1:
+            data = data.reshape(-1, 1)
+        
+        if cond is not None:
+            if self.ncond != cond.ndim:
+                raise RuntimeError("input cond dimention doesn't match with number of cond features!")
+            if self.ncond == 1:
+                cond = cond.reshape(-1, 1)
+            data = np.concatenate((data, cond), axis=1)
+        
+        x_train, x_test = train_test_split(data, test_size=0.33, shuffle=True)
+        
+        train_data = torch.utils.data.DataLoader(self.np_to_torch(x_train), batch_size=batch_size, shuffle=False, num_workers = 8, pin_memory = True)
+        val_data = torch.utils.data.DataLoader(self.np_to_torch(x_test), batch_size=batch_size, shuffle=False, num_workers = 8, pin_memory = True)
+        
+        return train_data, val_data
+        
+    
+    def train(self, data, cond=None, n_epochs=100, batch_size=512, seed=1, plot=False):
+        
+        update_epochs = 1
+        
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        
+        epochs, epochs_val = [], []
+        losses, losses_val = [], []
+        
+        train_data, val_data = self.process_data(data=data, batch_size=batch_size, cond=cond)
+        
+        for epoch in tqdm(range(n_epochs), ascii=' >='):
+            
+            losses_batch_per_e = []
+            
+            for batch_ndx, data in enumerate(train_data):
+                if cond is not None:
+                    x_ = data[:, :-self.ncond] if self.nfeat != 1 else data[:,0].reshape(-1, 1)
+                    c_ = data[:, -self.ncond:] if self.ncond != 1 else data[:,-1].reshape(-1, 1)
+                else:
+                    x_ = data
+                    c_ = None
+                loss = -self.flow.log_prob(inputs=x_, context=c_).mean()  
+                losses_batch_per_e.append(loss.detach().cpu().numpy())
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()  
+
+            epochs.append(epoch)
+            losses.append(np.mean(losses_batch_per_e))
+            
+            if epoch % update_epochs == 0: # validation loss
+                with torch.no_grad():
+
+                    val_losses_batch_per_e = []
+
+                    for batch_ndx, data in enumerate(val_data):
+                        self.optimizer.zero_grad()
+                        if cond is not None:
+                            x_ = data[:, :-self.ncond] if self.nfeat != 1 else data[:,0].reshape(-1, 1)
+                            c_ = data[:, -self.ncond:] if self.ncond != 1 else data[:,-1].reshape(-1, 1)
+                        else:
+                            x_ = data
+                            c_ = None
+                        val_loss = -self.flow.log_prob(inputs=x_, context=c_).mean() 
+                        val_losses_batch_per_e.append(val_loss.detach().cpu().numpy())
+               
+                epochs_val.append(epoch)
+                mean_val_loss = np.mean(val_losses_batch_per_e)
+                losses_val.append(mean_val_loss)
+            
+            if plot:
+                print(f"Epoch: {epoch} - loss: {loss} - val loss: {val_loss}")
+            
+        if plot:
+            plt.plot(losses, epochs, label="loss")
+            plt.plot(losses_val, epochs_val, label="val loss")
+            plt.legend()
+            plt.show
+                    
+
+    def sample(self, num_samples, cond=None):
+        samples = self.flow.sample(num_samples=num_samples, context=cond)
+        return samples.detach().numpy()
--- plotting.py
+++ plotting.py
@@ -10,12 +10,11 @@ def get_kl_div(p,q):
     return np.sum(div_arr)
 
 def plot_gen_full_bkg(samples, x1, x2):
-    sample2 = samples[:,0]
-    y1 = sample2[:,0]
-    y2 = sample2[:,1]
+    y1 = samples[:,0]
+    y2 = samples[:,1]
     plt.figure(figsize=(6,6))
     plt.scatter(x1, x2, alpha = 0.2, label = 'true bkg')
-    plt.scatter(y1, y2, alpha = 0.2, label = 'generated bkg')
+    plt.scatter(y1, y2, alpha = 0.2, color = "lightgreen", label = 'generated bkg')
     plt.axhline(y=1, color='r', linestyle='-')
     plt.axvline(x=1, color='r', linestyle='-')
     plt.legend()
@@ -25,12 +24,11 @@ def plot_gen_full_bkg(samples, x1, x2):
     plt.close
     
 def plot_gen_SRfromCR_bkg(samples, x1, x2):
-    sample2 = samples[:,0]
-    y1 = sample2[:,0]
-    y2 = sample2[:,1]
+    y1 = samples[:,0]
+    y2 = samples[:,1]
     plt.figure(figsize=(6,6))
     plt.scatter(x1, x2, alpha = 0.2, label = 'true bkg')
-    plt.scatter(y1, y2, alpha = 0.2, label = 'generated bkg')
+    plt.scatter(y1, y2, alpha = 0.2, color = "lightgreen", label = 'generated bkg in SR')
     plt.axhline(y=1, color='r', linestyle='-')
     plt.axvline(x=1, color='r', linestyle='-')
     plt.legend()
@@ -40,10 +38,10 @@ def plot_gen_SRfromCR_bkg(samples, x1, x2):
     plt.close
     
 def plot_gen_SR_bkg_in_y_random(samples, Y_SR):
-    sample2 = np.array(samples[:,0])
+    samples = np.array(samples)
     plt.figure(figsize=(6,4))
     bins = np.linspace(-5, 5, 50)
-    plt.hist(sample2, bins = bins, density = True, histtype='step', label='generated SR')
+    plt.hist(samples, bins = bins, density = True, histtype='step', label='generated SR')
     plt.hist(Y_SR, bins = bins, density = True, histtype='step', label='true SR')
     plt.title("Generated bkg distribution in y = random.")
     plt.legend()
@@ -52,31 +50,51 @@ def plot_gen_SR_bkg_in_y_random(samples, Y_SR):
     plt.close
     
 def plot_gen_SR_bkg_in_y_cond(samples, Y_SR, k, q):
-    sample2 = np.array(samples[:,0])
+    samples = np.array(samples)
     bins = np.linspace(-5, 5, 50)
     plt.figure(figsize=(6,4))
-    plt.hist(sample2, bins = bins, density = True, histtype='step', label='generated SR')
+    plt.hist(samples, bins = bins, density = True, histtype='step', label='generated SR')
     plt.hist(Y_SR, bins = bins, density = True, histtype='step', label='true SR')
     plt.title(f"Generated bkg in y = N(${k}\\alpha$+{q}$\\beta$, 1)")
     plt.legend()
     plt.show
     plt.savefig(f'plots/gen_SR_bkg_in_y_cond{k*10}.png')
     plt.close
-    
-def plot_gen_SR_bkg_in_y_cond_list(samples_list, Y_SR, k, q):
+
+def plot_bkg_in_y_cond_list(Y_list, Y_list2, Y_label, Y_label2, k, q):
     colors = ['blue', 'green', 'slategrey', 'steelblue']
-    sample2 = [] 
-    for samples in samples_list:
-        sample2.append(np.array(samples[:,0]))
-    N = len(sample2)
-    if N==len(Y_SR) and N==len(k) and N==len(q) and N<len(colors):
+    N = len(Y_list)
+    if N==len(Y_list2) and N==len(k) and N==len(q) and N<=len(colors):
         bins = np.linspace(-6, 8, 50)
         fig, ax1 = plt.subplots(figsize=(12,8))
         for i in range(N):
-            c0, cbins, _ = ax1.hist(Y_SR[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"true SR, k={k[i]} q={q[i]}")
-            c1, cbins, _ = ax1.hist(sample2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"generated SR, k={k[i]} q={q[i]}")
+            c0, cbins, _ = ax1.hist(Y_list[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"{Y_label}, k={k[i]} q={q[i]}")
+            c1, cbins, _ = ax1.hist(Y_list2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"{Y_label2}, k={k[i]} q={q[i]}")
             kl_div = get_kl_div(c0,c1)
-            ax1.hist(sample2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"kl div={kl_div:.3f}")
+            ax1.hist(Y_list2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"kl div={kl_div:.3f}")
+        ax1.set_title("Background in y = Normal(mean = $k\\alpha$+k$\\beta$, $\sigma$ = 1)", fontsize = 14)
+        ax1.set_xlabel("y")
+        plt.legend(loc='upper left', fontsize = 14)
+        plt.show
+        plt.savefig(f"plots/{Y_label}_{Y_label2}_in_y_cond.png")
+        plt.close
+    else:
+        print("Wrong input lists!")
+    
+def plot_gen_SR_bkg_in_y_cond_list(samples_list, Y_SR, k, q):
+    colors = ['blue', 'green', 'slategrey', 'steelblue']
+    samples = [] 
+    for samp in samples_list:
+        samples.append(np.array(samp))
+    N = len(samples)
+    if N==len(Y_SR) and N==len(k) and N==len(q) and N<=len(colors):
+        bins = np.linspace(-6, 8, 50)
+        fig, ax1 = plt.subplots(figsize=(12,8))
+        for i in range(N):
+            c0, cbins, _ = ax1.hist(Y_SR[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"true SR, k={k[i]} q={q[i]}")
+            c1, cbins, _ = ax1.hist(samples[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"generated SR, k={k[i]} q={q[i]}")
+            kl_div = get_kl_div(c0,c1)
+            ax1.hist(Y_SR[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"kl div={kl_div:.3f}")
         ax1.set_title("Generated bkg in y = Normal(mean = $k\\alpha$+q$\\beta$, $\\sigma$ = 1)", fontsize = 14)
         ax1.set_xlabel("y")
         plt.legend(loc='upper left', fontsize = 14)
