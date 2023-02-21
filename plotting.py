c944d5b78cf3a11b49d090d06eb3349a6e1ac132
Kehang Bai <kehangbai@gmail.com>
2023-02-20 22:59:04 -0800

    clean up

--- plotting.py
+++ plotting.py
@@ -61,45 +61,38 @@ def plot_gen_SR_bkg_in_y_cond(samples, Y_SR, k, q):
     plt.savefig(f'plots/gen_SR_bkg_in_y_cond{k*10}.pdf')
     plt.close
 
-def plot_bkg_in_y_cond_list(Y_list, Y_list2, Y_label, Y_label2, k, q):
+def plot_kl_div(Y_list, Y_list2, Y_label, Y_label2, k, q=None):
     colors = ['blue', 'green', 'slategrey', 'steelblue']
-    N = len(Y_list)
-    if N==len(Y_list2) and N==len(k) and N==len(q) and N<=len(colors):
-        bins = np.linspace(-6, 8, 50)
-        fig, ax1 = plt.subplots(figsize=(12,8))
-        for i in range(N):
-            c0, cbins, _ = ax1.hist(Y_list[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"{Y_label}, k={k[i]} q={q[i]}")
-            c1, cbins, _ = ax1.hist(Y_list2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"{Y_label2}, k={k[i]} q={q[i]}")
-            kl_div = get_kl_div(c0,c1)
-            ax1.hist(Y_list2[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"kl div={kl_div:.3f}")
-        ax1.set_title("Background in y = Normal(mean = $k\\alpha$+k$\\beta$, $\sigma$ = 1)", fontsize = 14)
-        ax1.set_xlabel("y")
-        plt.legend(loc='upper left', fontsize = 14)
-        plt.show
-        plt.savefig(f"plots/{Y_label}_{Y_label2}_in_y_cond.pdf")
-        plt.close
-    else:
-        print("Wrong input lists!")
     
-def plot_gen_SR_bkg_in_y_cond_list(samples_list, Y_SR, k, q):
-    colors = ['blue', 'green', 'slategrey', 'steelblue']
-    samples = [] 
-    for samp in samples_list:
-        samples.append(np.array(samp))
-    N = len(samples)
-    if N==len(Y_SR) and N==len(k) and N==len(q) and N<=len(colors):
+    N = len(Y_list)
+    
+    if N==len(Y_list2) and N==len(k) and N<=len(colors):
         bins = np.linspace(-6, 8, 50)
         fig, ax1 = plt.subplots(figsize=(12,8))
+        
         for i in range(N):
-            c0, cbins, _ = ax1.hist(Y_SR[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"true SR, k={k[i]} q={q[i]}")
-            c1, cbins, _ = ax1.hist(samples[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"generated SR, k={k[i]} q={q[i]}")
+            
+            if q is None:
+                label_kq = f"k={k[i]}"
+                title_kq = "$k\\alpha$"
+                tag = "1DSR"
+            elif N==len(q):
+                label_kq = f"k={k[i]} q={q[i]}"
+                title_kq = "$k\\alpha$+q$\\beta$"
+                tag = "2DSR"
+            else:
+                print("Wrong q lists!")
+                break
+            
+            c0, cbins, _ = ax1.hist(Y_list[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"{Y_label}, {label_kq}")
+            c1, cbins, _ = ax1.hist(Y_list2[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"{Y_label2}, {label_kq}")
             kl_div = get_kl_div(c0,c1)
-            ax1.hist(Y_SR[i], bins = bins, density = True, histtype='step', color=colors[i], label=f"kl div={kl_div:.3f}")
-        ax1.set_title("Generated bkg in y = Normal(mean = $k\\alpha$+q$\\beta$, $\\sigma$ = 1)", fontsize = 14)
+            ax1.hist(Y_list2[i], bins = bins, density = True, histtype='stepfilled', alpha = 0.5, color=f"light{colors[i]}", label=f"kl div={kl_div:.3f}")
+        ax1.set_title(f"Background in y = Normal(mean = {title_kq}, $\sigma$ = 1)", fontsize = 14)
         ax1.set_xlabel("y")
         plt.legend(loc='upper left', fontsize = 14)
         plt.show
-        plt.savefig('plots/gen_SR_bkg_in_y_cond.pdf')
+        plt.savefig(f"plots/{Y_label}_{Y_label2}_in_y_{tag}.pdf")
         plt.close
     else:
         print("Wrong input lists!")
