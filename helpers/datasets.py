b43464b1d20cd478b792fe7f9fe4b39ee72c83c1
Radha <radha.mastandrea@gmail.com>
2023-03-14 13:15:44 -0700

    finished the cathode example, working on feta

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -1,50 +1,61 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
 import pytorch_lightning as pl
 
 import numpy as np
+import numpy.ma as ma
 
 
 class ConditionalDataset(Dataset):
-    def __init__(self, data, context):
+    def __init__(self, features, context):
         
-        self.data = data
+        self.features = features
         self.context = context
         
-        self.dim_data = data.shape[-1]
-        self.dim_cont = context.shape[-1]
+        self.dim_features = features.shape[-1]
+        self.dim_context = context.shape[-1]
     
     def __len__(self):
-        return len(self.data)
+        return len(self.features)
 
     def __getitem__(self, idx):
-        data_p = self.data[idx]
+        feature_p = self.features[idx]
         context_p = self.context[idx]
-        return torch.tensor(data_p).float(), torch.tensor(context_p).float()
+        return torch.tensor(feature_p), torch.tensor(context_p)
     
     
-  
+# function to generate the toy data
+def Y(k, theta, x1, x2, sigma=1):
+    return np.random.normal(k*(np.cos(theta)*x1 + np.sin(theta)*x2), sigma).astype(dtype=np.float32)
     
-class ToyDataModuleLit(pl.LightningDataModule):
-    def __init__(self, dim_data, dim_context, num_train, num_val):
+"""
+For a CATHODE-style analysis
+"""
+
+class ToySingleDataModuleLit(pl.LightningDataModule):
+    def __init__(self, dim_context, num_samples, k, theta):
         super().__init__()
         
-        self.dim_data = dim_data
         self.dim_context = dim_context
-        self.num_train = num_train
-        self.num_val = num_val
+        self.num_samples = num_samples
+        self.k = k
+        self.theta = theta
           
-        self.batch_size = 32
+        self.batch_size = 128
         
-        self.train_data = np.random.triangular(-2, -1, 3, size = (self.num_train, self.dim_data))
-        self.train_context = np.random.normal(4, 5, size = (self.num_train, self.dim_context))
+        # generate the contexts
+        self.context = np.random.normal(0, 1, size = (self.num_samples, self.dim_context)).astype(dtype=np.float32)
+        
+        # generate the features
+        if self.dim_context == 1:
+            self.features = Y(self.k, self.theta, self.context, self.context)
 
-        self.valid_data = np.random.triangular(-2, -1, 3, size = (self.num_val, self.dim_data))
-        self.valid_context = np.random.normal(4, 5, size = (self.num_val, self.dim_context))
+        elif self.dim_context == 2:
+            self.features = Y(self.k, self.theta, self.context[:,0], self.context[:,1])
+        else:
+            raise Exception("Toy dataset only implemented for dim_context = 1, 2.")
         
-        self.train_dataset = ConditionalDataset(self.train_data, self.train_context)
-        self.valid_dataset = ConditionalDataset(self.valid_data, self.valid_context)
-          
         
     def prepare_data(self):
 
@@ -52,20 +63,112 @@ class ToyDataModuleLit(pl.LightningDataModule):
   
     def setup(self, stage=None):
         
+        # generate the context masks
+        if self.dim_context == 1:
+            mask_CR = self.context < 1
+            mask_SR = self.context > 1
+        
+        elif self.dim_context == 2:
+            mask_CR = np.logical_not((self.context[:,0] > 1) & (self.context[:,1] > 1))
+            mask_SR = (self.context[:,0] > 1) & (self.context[:,1] > 1)
+            
+        else:
+            raise Exception("Toy dataset only implemented for dim_context = 1, 2.")
+            
+        
+        # split into train-val
+        features_train, features_valid, context_train, context_valid, mask_CR_train, mask_CR_valid, mask_SR_train, mask_SR_valid = train_test_split(self.features, self.context, mask_CR, mask_SR, test_size=0.2)
+
+        # make datasets
+        self.train_dataset_CR = ConditionalDataset(features_train[mask_CR_train].reshape(-1, 1), context_train[mask_CR_train].reshape(-1, self.dim_context))
+        self.valid_dataset_CR = ConditionalDataset(features_valid[mask_CR_valid].reshape(-1, 1), context_valid[mask_CR_valid].reshape(-1, self.dim_context))
+        self.train_dataset_SR = ConditionalDataset(features_train[mask_SR_train].reshape(-1, 1), context_train[mask_SR_train].reshape(-1, self.dim_context))
+        self.valid_dataset_SR = ConditionalDataset(features_valid[mask_SR_valid].reshape(-1, 1), context_valid[mask_SR_valid].reshape(-1, self.dim_context))
+          
+  
+    def train_dataloader(self):
+    
+        return DataLoader(self.train_dataset_CR, 
+                      batch_size = self.batch_size, shuffle = True,
+                     num_workers = 8, pin_memory = True)
+       
+  
+    def val_dataloader(self):
+
+        return DataLoader(self.valid_dataset_CR,
+                          batch_size = self.batch_size, shuffle = False,
+                          num_workers = 8, pin_memory = True)
+    
+"""
+For a FETA-style analysis
+"""
+
+    
+class ToyJointDataModuleLit(pl.LightningDataModule):
+    def __init__(self, dim_context, num_samples, k, theta):
+        super().__init__()
+        
+        self.dim_context = dim_context
+        self.num_samples = num_samples
+        self.k = k
+        self.theta = theta
+          
+        self.batch_size = 128
+        
+        # generate the contexts
+        self.context = np.random.normal(0, 1, size = (self.num_samples, self.dim_context)).astype(dtype=np.float32)
+        
+        # generate the features
+        if self.dim_context == 1:
+            self.features = Y(self.k, self.theta, self.context, self.context)
+
+        elif self.dim_context == 2:
+            self.features = Y(self.k, self.theta, self.context[:,0], self.context[:,1])
+        else:
+            raise Exception("Toy dataset only implemented for dim_context = 1, 2.")
+        
+        
+    def prepare_data(self):
+
         pass
+  
+    def setup(self, stage=None):
+        
+        # generate the context masks
+        if self.dim_context == 1:
+            mask_CR = self.context < 1
+            mask_SR = self.context > 1
+        
+        elif self.dim_context == 2:
+            mask_CR = np.logical_not((self.context[:,0] > 1) & (self.context[:,1] > 1))
+            mask_SR = (self.context[:,0] > 1) & (self.context[:,1] > 1)
+            
+        else:
+            raise Exception("Toy dataset only implemented for dim_context = 1, 2.")
+            
+        
+        # split into train-val
+        features_train, features_valid, context_train, context_valid, mask_CR_train, mask_CR_valid, mask_SR_train, mask_SR_valid = train_test_split(self.features, self.context, mask_CR, mask_SR, test_size=0.2)
+
+        # make datasets
+        self.train_dataset_CR = ConditionalDataset(features_train[mask_CR_train].reshape(-1, 1), context_train[mask_CR_train].reshape(-1, self.dim_context))
+        self.valid_dataset_CR = ConditionalDataset(features_valid[mask_CR_valid].reshape(-1, 1), context_valid[mask_CR_valid].reshape(-1, self.dim_context))
+        self.train_dataset_SR = ConditionalDataset(features_train[mask_SR_train].reshape(-1, 1), context_train[mask_SR_train].reshape(-1, self.dim_context))
+        self.valid_dataset_SR = ConditionalDataset(features_valid[mask_SR_valid].reshape(-1, 1), context_valid[mask_SR_valid].reshape(-1, self.dim_context))
+          
   
     def train_dataloader(self):
         
-        # Generating train_dataloader
-        return DataLoader(self.train_dataset, 
-                          batch_size = self.batch_size, shuffle = True,
-                         num_workers = 8, pin_memory = True)
+
+        return DataLoader(self.train_dataset_CR, 
+                      batch_size = self.batch_size, shuffle = True,
+                     num_workers = 8, pin_memory = True)
+       
   
     def val_dataloader(self):
-        
-          # Generating val_dataloader
-        return DataLoader(self.valid_dataset,
+
+        return DataLoader(self.valid_dataset_CR,
                           batch_size = self.batch_size, shuffle = False,
                           num_workers = 8, pin_memory = True)
-
-  
+   
+   
--- helpers/models.py
+++ helpers/models.py
@@ -13,32 +13,40 @@ from torch import optim
 
 
 # define the Base Density trainer
-class BaseDensityLit(pl.LightningModule):
-    def __init__(self, dim_data, dim_cont, flow_args, lr, wd):
+class FlowLIT(pl.LightningModule):
+    def __init__(self, dim_features, dim_context, flow_args, lr, wd, base_dist = None):
         super().__init__()
         
         self.lr = lr
         self.wd = wd
         
-        self.dim_data = dim_data
-        self.dim_cont = dim_cont
+        self.dim_features = dim_features
+        self.dim_context = dim_context
         self.num_layers = flow_args["num_layers"]
         self.num_nodes = flow_args["num_nodes"]
         self.num_blocks = flow_args["num_blocks"]
         self.num_bins = flow_args["num_bins"]
         
         self.tails = "linear"
-        self.tail_bound = 3.5
-        self.base_dist = StandardNormal(shape=[self.dim_data])
+        self.tail_bound = 7
+        
+        if base_dist is not None:
+            self.base_dist = base_dist
+            # set the base flow to be static
+            for param in model.parameters():
+                param.requires_grad = False
+                
+        else:
+            self.base_dist = StandardNormal(shape=[self.dim_features])
         
         # make the transforms
         
         self.transforms_list = []
         for _ in range(self.num_layers):
-            self.transforms_list.append(MaskedPiecewiseRationalQuadraticAutoregressiveTransform(features = self.dim_data, 
+            self.transforms_list.append(MaskedPiecewiseRationalQuadraticAutoregressiveTransform(features = self.dim_features, 
                     hidden_features = self.num_nodes, num_blocks = self.num_blocks, tail_bound = self.tail_bound, 
-                    context_features = self.dim_cont, tails = self.tails, num_bins = self.num_bins))     
-            self.transforms_list.append(ReversePermutation(features = self.dim_data)) 
+                    context_features = self.dim_context, tails = self.tails, num_bins = self.num_bins))     
+            self.transforms_list.append(ReversePermutation(features = self.dim_features)) 
             
         self.transform = CompositeTransform(self.transforms_list)     
         self.flow = Flow(self.transform, self.base_dist)    
@@ -64,3 +72,6 @@ class BaseDensityLit(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.flow.parameters(), lr = self.lr, weight_decay = self.wd)
         return optimizer
+    
+    
+    
