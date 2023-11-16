875db5cff1bf8dd25f60117bc4d6022d7e752673
kehangbai <kehang.bai@cern.ch>
2023-11-16 01:31:08 -0800

    update bash script

--- run_full_AD.sh
+++ run_full_AD.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-options=("CATHODE" "FETA" "SALAD" "idealAD")
+# options=("CATHODE" "FETA" "SALAD" "idealAD")
+options=("FETA")
 
 
 task_reweighting () {
 
     local run=$1
-    echo "run test $run."
+    echo "run reweighting $run."
     
     directory="dataset_$run"
 
@@ -19,8 +20,9 @@ task_reweighting () {
     for file_path in "${directory}/inputs_s"*.npz; do
         file_name=$(basename "$file_path")
         echo "${directory}/$file_name"
-
-        "run-reweighting" -i "${directory}/${file_name}" -o "${directory}/reweighting/run${num}"
+        if ((num>=7)); then
+            "run-reweighting" -i "${directory}/${file_name}" -o "${directory}/reweighting/run${num}" -c ../configs/classifier.yml
+        fi
         ((num++))
 
     done
@@ -34,10 +36,6 @@ task_AD () {
     
     directory="dataset_$run"
 
-    ####################
-    # run extrapoation #
-    ####################
-
 
     for option_name in "${options[@]}"; do
 
@@ -47,33 +45,19 @@ task_AD () {
             file_name=$(basename "$file_path")
             echo "${directory}/$file_name"
 
-            if [ "${option_name}" != "CATHODE" ] && [ "${option_name}" != "FETA" ]; then
-                "run-${option_name}" -i "${directory}/${file_name}"  -o "${directory}/${option_name}/run${num}"
-            else
-                "run-${option_name}" -i "${directory}/${file_name}"  -w "${directory}/reweighting/run${num}/weights.npz" -o "${directory}/${option_name}/run${num}"    
-            fi
+            if ((num>=7)); then
+
+                if [ "${option_name}" != "CATHODE" ] && [ "${option_name}" != "FETA" ]; then
+                    "run-${option_name}" -i "${directory}/${file_name}"  -o "${directory}/${option_name}/run${num}" -t 10
+                else
+                    "run-${option_name}" -i "${directory}/${file_name}"  -w "${directory}/reweighting/run${num}/weights.npz" -c ../configs/classifier_AD.yml -o "${directory}/${option_name}/run${num}"
+                fi
+
+                echo "Evaluate ${option_name} run${num}."
+
+                run-evaAD -i "test_dataset/test_inputs.npz" -o "${directory}/${option_name}/run${num}" -n "${option_name}" -v
             
-            ((num++))
-
-        done
-
-    done
-
-
-    ##################
-    # run evaluation #
-    ##################
-
-
-    for option_name in "${options[@]}"; do
-
-        num=0
-
-        for file_path in "${directory}/inputs_s"*.npz; do
-            echo "${directory}/$file_name"
-            echo "Evaluate ${option_name} run${num}."
-
-            run-evaAD -i "test_dataset/test_inputs.npz" -o "${directory}/${option_name}/run${num}" -n "${option_name}"
+            fi
 
             ((num++))
 
@@ -81,33 +65,32 @@ task_AD () {
 
         echo "plot ${option_name}."
 
-        plt-sig-inj -i "${directory}" -r "${directory}/${option_name}" -o "${directory}/plot_sig_inj_${option_name}" -n "${option_name}"
-
+        plt-sig-inj -i "${directory}" -n "${option_name}"
 
     done
+
 
 }
 
 
 task_supervised () {
 
-    local run=$1
-    echo "run test $run."
-
     directory="supervised_dataset"
 
-    file_name="supervised_inputs_${run}.npz"
+    file_name="supervised_inputs.npz"
     echo "$file_name"
 
-    run-supervised -i "${directory}/${file_name}"  -o "${directory}/run${run}" -t 10
+    run-supervised -i "${directory}/${file_name}"  -o "${directory}" -c ../configs/classifier.yml -t 10
 
     # Evaluate AD classifier
-    echo "Evaluate supervised run ${run}."
+    echo "Evaluate supervised."
 
-    run-evaAD -i "test_dataset/test_inputs.npz" -o "${directory}/run${run}" -n supervised
+    run-evaAD -i "test_dataset/test_inputs.npz" -o "${directory}" -n supervised
 
 }
 
 
-
-# task_xxx 0
+# $ make datatset_0
+# $ gen-toy-sd -o dataset_0
+### run a task for dataset_0
+task_AD 0
