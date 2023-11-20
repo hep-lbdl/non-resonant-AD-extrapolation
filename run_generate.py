874c1ceb7c52253dccf6c26e80a2129216c083e9
rmastand <radha.mastandrea@gmail.com>
2023-11-20 05:19:59 -0800

    updated scripts for generate, reweight methods

---
+++ run_generate.py
@@ -0,0 +1,158 @@
+import argparse
+import numpy as np
+from helpers.SimpleMAF import SimpleMAF
+import torch
+import os
+import logging
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-i",
+    "--input",
+    help="home folder for input training samples and conditional inputs",
+    default="/global/cfs/cdirs/m3246/rmastand/bkg_extrap/redo/data/"
+)
+
+parser.add_argument(
+    "-s",
+    "--signal",
+    default=None,
+    help="signal fraction",
+)
+
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Generate flow config file",
+    default="configs/generate_physics.yml"
+)
+
+parser.add_argument(
+    '-l', 
+    "--load_model",
+    default=False,
+    help='Load best trained model.'
+)
+
+parser.add_argument(
+    '-m', 
+    "--model_path",
+    help='Path to best trained model'
+)
+
+parser.add_argument(
+    "--oversample",
+    default=1,
+    help="Oversampling",
+)
+parser.add_argument(
+    "-o",
+    "--outdir",
+    help="output directory",
+    default="/global/cfs/cdirs/m3246/rmastand/bkg_extrap/redo/"
+)
+
+parser.add_argument(
+    "-v",
+    "--verbose",
+    default=False,
+    help="Verbose enable DEBUG",
+)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+
+log_level = logging.DEBUG if args.verbose else logging.INFO
+    
+log = logging.getLogger("run")
+log.setLevel(log_level)
+
+def main():
+
+    # selecting appropriate device
+    CUDA = torch.cuda.is_available()
+    print("cuda available:", CUDA)
+    device = torch.device("cuda" if CUDA else "cpu")
+    
+    model_dir = f"{args.outdir}/models/"
+    samples_dir = f"{args.outdir}/samples/"
+    os.makedirs(model_dir, exist_ok=True)
+    os.makedirs(samples_dir, exist_ok=True)
+        
+    # load input files
+    data_events = np.load(f"{args.input}/data_{args.signal}.npz")
+    data_events_cr = data_events["data_events_cr"]
+    data_events_sr = data_events["data_events_sr"]
+    
+    mc_events = np.load(f"{args.input}/mc_events.npz")
+    mc_events_sr = mc_events["mc_events_sr"]
+    
+    print("Working with s/b = ", args.signal, ". CR has", len(data_events_cr), "events, SR has", len(data_events_sr), "events.")
+
+    # Train flow in the CR
+    # To do the closure tests, we need to withhold a small amount of CR data
+    n_withold = 10000 
+    n_context = 2
+    n_features = 5
+    
+    data_context_cr_train = data_events_cr[:-n_withold,:n_context]
+    data_context_cr_test = data_events_cr[-n_withold:,:n_context]
+    data_feature_cr_train = data_events_cr[:-n_withold,n_context:]
+    data_feature_cr_test = data_events_cr[-n_withold:,n_context:]
+    
+    mc_context_sr = mc_events_sr[:,:n_context]
+    mc_feature_sr = mc_events_sr[:,n_context:]
+    
+    if args.config is not None:
+        with open(args.config, 'r') as stream:
+            params = yaml.safe_load(stream)
+            n_layers = params["n_layers"]
+            n_hidden_features = params["n_hidden_features"]
+            learning_rate = params["learning_rate"]
+            batch_size = params["batch_size"]
+            n_epochs = params["n_epochs"]
+    else:
+        n_layers = 1
+        n_hidden_features = 8
+        learning_rate = 0.0001
+        batch_size = 256
+        n_epochs = 20
+         
+    # efine the flow
+    MAF = SimpleMAF(num_features=n_features, num_context=n_context, device=device, num_layers=n_layers, num_hidden_features=n_hidden_features, learning_rate = learning_rate)
+    
+    # Model in
+    load_model = args.load_model
+    
+    if load_model:
+        # Check if a model exist
+        if os.path.isfile(args.model_path):
+            # Load the trained model
+            print("Loading in model...")
+            MAF = torch.load(args.model_path)
+            MAF.to(device)
+        else:
+            load_model = False
+
+    if not load_model:   
+        print("Training Generate model...")
+
+        MAF.train(data=data_feature_cr_train, cond=data_context_cr_train, batch_size=batch_size, n_epochs=n_epochs, outdir=model_dir, save_model=True, model_name="generate_best")
+        print("Done training!")
+
+    # sample CR from data
+    pred_bkg_CR = MAF.sample(1, data_context_cr_test)
+    np.savez(f"{samples_dir}/generate_CR_closure_s{args.signal}.npz", target_cr=data_feature_cr_test, generate_cr=pred_bkg_CR)
+
+    # sample from MAF
+    n_sample = 1 if args.oversample else 1
+    pred_bkg_SR = MAF.sample(n_sample, mc_context_sr)
+
+    # save generated samples
+    np.savez(f"{samples_dir}/generate_SR_s{args.signal}.npz", samples = pred_bkg_SR)
+    
+    print("All done.")
+
+if __name__ == "__main__":
+    main()
---
+++ run_reweight.py
@@ -0,0 +1,177 @@
+import argparse
+import numpy as np
+from helpers.Classifier import Classifier
+import torch
+import os
+import logging
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-i",
+    "--input",
+    help="home folder for input training samples and conditional inputs",
+    default="/global/cfs/cdirs/m3246/rmastand/bkg_extrap/redo/data/"
+)
+
+parser.add_argument(
+    "-s",
+    "--signal",
+    default=None,
+    help="signal fraction",
+)
+
+parser.add_argument(
+    "-c",
+    "--config",
+    help="Reweight NN config file",
+    default="configs/reweight_physics.yml"
+)
+
+parser.add_argument(
+    '-l', 
+    "--load_model",
+    default=False,
+    help='Load best trained model.'
+)
+
+parser.add_argument(
+    '-m', 
+    "--model_path",
+    help='Path to best trained model'
+)
+
+
+parser.add_argument(
+    "-o",
+    "--outdir",
+    help="output directory",
+    default="/global/cfs/cdirs/m3246/rmastand/bkg_extrap/redo/"
+)
+
+parser.add_argument(
+    "-v",
+    "--verbose",
+    default=False,
+    help="Verbose enable DEBUG",
+)
+args = parser.parse_args()
+
+
+logging.basicConfig(level=logging.INFO)
+
+log_level = logging.DEBUG if args.verbose else logging.INFO
+    
+log = logging.getLogger("run")
+log.setLevel(log_level)
+
+
+def main():
+
+    # selecting appropriate device
+    CUDA = torch.cuda.is_available()
+    print("cuda available:", CUDA)
+    device = torch.device("cuda" if CUDA else "cpu")
+    
+    model_dir = f"{args.outdir}/models/"
+    samples_dir = f"{args.outdir}/samples/"
+    os.makedirs(model_dir, exist_ok=True)
+    os.makedirs(samples_dir, exist_ok=True)
+        
+    # load input files
+    data_events = np.load(f"{args.input}/data_{args.signal}.npz")
+    data_events_cr = data_events["data_events_cr"]
+    data_events_sr = data_events["data_events_sr"]
+    
+    mc_events = np.load(f"{args.input}/mc_events.npz")
+    mc_events_cr = mc_events["mc_events_cr"]
+    mc_events_sr = mc_events["mc_events_sr"]
+    
+    print("Working with s/b = ", args.signal, ". CR has", len(data_events_cr), "events, SR has", len(data_events_sr), "events.")
+
+    # Train flow in the CR
+    # To do the closure tests, we need to withhold a small amount of CR data
+    n_withold = 10000 
+    
+    data_cr_train = data_events_cr[:-n_withold]
+    data_cr_test = data_events_cr[-n_withold:]
+    mc_cr_train = mc_events_cr[:-n_withold]
+    mc_cr_test = mc_events_cr[-n_withold:]
+
+    input_x_train_CR = np.concatenate([mc_cr_train, data_cr_train], axis=0)
+    # create labels for classifier
+    mc_cr_label = np.zeros(mc_cr_train.shape[0]).reshape(-1,1)
+    data_xr_label = np.ones(data_cr_train.shape[0]).reshape(-1,1)
+    input_y_train_CR = np.concatenate([MC_CR_label, data_CR_label], axis=0)
+
+    if args.config is not None:
+        with open(args.config, 'r') as stream:
+            params = yaml.safe_load(stream)
+            layers = params["layers"]
+            learning_rate = params["learning_rate"]
+            batch_size = params["batch_size"]
+            n_epochs = params["n_epochs"]
+    else:
+        layers = [32,32]
+        learning_rate = 0.0001
+        batch_size = 512
+        n_epochs = 50
+        
+    # Define the network
+     NN_reweight = Classifier(n_inputs=7, layers=layers, learning_rate=learning_rate, device=device, outdir=model_dir)
+        
+    # Model in
+    load_model = args.load_model
+    
+    if load_model:
+        # Check if a model exist
+        if os.path.isfile(args.model_path):
+            # Load the trained model
+            print("Loading in model...")
+            NN_reweight = torch.load(args.model_path)
+            NN_reweight.to(device)
+        else:
+            load_model = False
+
+    if not load_model:   
+        print("Training Reweight model...")
+        NN_reweight_train.train(input_x_train_CR, input_y_train_CR, save_model=True, batch_size=batch_size, n_epochs=n_epochs)
+        print("Done training!")
+        
+    # evaluate weights in CR
+    w_CR = NN_reweight.evaluation(MC_CR_test_SALAD)
+    w_CR = (w_CR/(1.-w_CR)).flatten()
+
+
+
+    np.save(f"{sampled_ourdir}/SALAD_full_MC_CR_test_{s_inj}", MC_CR_test_SALAD)
+    np.save(f"{sampled_ourdir}/SALAD_full_w_CR_test_{s_inj}", w_CR)
+    np.save(f"{sampled_ourdir}/SALAD_full_data_CR_test_{s_inj}", data_CR_test_SALAD)
+
+
+    # ## Generate SR samples
+
+    # In[7]:
+
+
+    model_path = f"{model_outdir}/SALAD_full_{s_inj}.pt"
+    NN_reweight = torch.load(f"{model_path}")
+    NN_reweight.to(device)
+
+    # evaluate weights in CR
+    w_cr = NN_reweight.evaluation(mc_cr_test)
+    w_cr = (w_cr/(1.-w_cr)).flatten()
+    np.savez(f"{samples_dir}/reweight_CR_closure_s{args.signal}.npz", target_cr=data_cr_test, mc_cr=mc_cr_test, w_cr=w_cr)
+
+    # evaluate weights in SR
+    w_sr = NN_reweight.evaluation(mc_events_sr)
+    w_sr = (w_sr/(1.-w_sr)).flatten()
+    np.savez(f"{samples_dir}/reweight_SR_s{args.signal}.npz", mc_samples=mc_events_sr, w_sr=w_sr)
+    
+    print("All done.")
+  
+
+     
+    
+if __name__ == "__main__":
+    main()
